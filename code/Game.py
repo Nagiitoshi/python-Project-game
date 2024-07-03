468fce8874e380823f7aac85c39c7d044b147b38
Nagiitoshi <126087189+Nagiitoshi@users.noreply.github.com>
2024-07-02 21:15:21 -0300

    Text Menu

--- code/Game.py
+++ code/Game.py
@@ -2,15 +2,20 @@
 # -*- coding: utf-8 -*-
 import pygame
 
+
 from code.Menu import Menu
+from code.const import WIN_WIDTH, WIN_HEIGHT
 
 
 class Game:
     def __init__(self):
-        self.window = pygame.display.set_mode(size=(600, 480))
+        pygame.init()
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
 
-    def run(self, ):
+    def run(self):
+
         while True:
             menu = Menu(self.window)
+
             menu.run()
-            pass
+

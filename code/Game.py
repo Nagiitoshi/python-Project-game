0606c9c1765de72d97cda8cd1a53bdd328478e7f
Nagiitoshi <126087189+Nagiitoshi@users.noreply.github.com>
2024-07-09 21:14:10 -0300

    Aula sobre criação de movimento de entidades

--- code/Game.py
+++ code/Game.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import sys
+
 import pygame
 
-
+from code.Level import Level
 from code.Menu import Menu
-from code.const import WIN_WIDTH, WIN_HEIGHT
+from code.const import WIN_WIDTH, WIN_HEIGHT, MENU_OPTION
 
 
 class Game:
@@ -16,6 +18,14 @@ class Game:
 
         while True:
             menu = Menu(self.window)
+            menu_return = menu.run()
 
-            menu.run()
+            if menu_return in [MENU_OPTION[0], MENU_OPTION[1], MENU_OPTION[2]]:
+                level = Level(self.window, 'Level1', menu_return)
+                level_return = level.run()
+            else:
+                pygame.quit()
+                sys.exit()
+
+
 
--- code/Menu.py
+++ code/Menu.py
@@ -1,27 +1,54 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import sys
+
 import pygame.image
 from pygame import Surface, Rect
 from pygame.font import Font
 
-from code.const import WIN_WIDTH
+from code.const import WIN_WIDTH, COLOR_ORANGE, MENU_OPTION, COLOR_WHITE, COLOR_YELLOW
 
 
 class Menu:
     def __init__(self, window):
         self.window: Surface = window
-        self.surf = pygame.image.load('/home/endil/PycharmProjects/python-Project-facul/dowload/Menu.png')
-        self.rect = self.surf.get_rect(left=0, top=0)
+        self.surf = pygame.image.load('/home/endil/PycharmProjects/python-Project-facul/dowload/re:zeroRem.jpeg')
+        self.rect = self.surf.get_rect(left=-200, top=-100)
 
     def run(self, ):
         pygame.mixer_music.load('/home/endil/PycharmProjects/python-Project-facul/dowload/menu_music.mp3')
         pygame.mixer_music.play(-1)
-
+        menu_option = 0
+        # desenhar na tela
         while True:
             self.window.blit(source=self.surf, dest=self.rect)
-            self.menu_text(50, "Royal Room", (255, 128, 0), ((WIN_WIDTH / 2), 70))
-            self.menu_text(50, "Shooter", (255, 128, 0), ((WIN_WIDTH / 2), 70))
+            self.menu_text(50, "Royal Room", COLOR_ORANGE, ((WIN_WIDTH / 2), 70))
+            self.menu_text(50, "Shooter", COLOR_ORANGE, ((WIN_WIDTH / 2), 120))
+
+            for i in range(len(MENU_OPTION)):
+                if i == menu_option:
+                    self.menu_text(20, MENU_OPTION[i], COLOR_YELLOW, ((WIN_WIDTH / 2), 200 + 30 * i))
+                else:
+                    self.menu_text(20, MENU_OPTION[i], COLOR_WHITE, ((WIN_WIDTH / 2), 200 + 30 * i))
             pygame.display.flip()
+            # verificar eventos
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.KEYDOWN:  # testar se algume tecla foi pressionada
+                    if event.key == pygame.K_DOWN:  # se a tecla seta para baixo foi pressionada
+                        if menu_option < len(MENU_OPTION) - 1:
+                            menu_option += 1
+                        else:
+                            menu_option = 0
+                    if event.key == pygame.K_UP:  # se a tecla seta para baixo foi pressionada
+                        if menu_option > 0:
+                            menu_option -= 1
+                        else:
+                            menu_option = len(MENU_OPTION) - 1
+                    if event.key == pygame.K_RETURN:
+                        return MENU_OPTION[menu_option]
 
     def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
         text_font: Font = pygame.font.SysFont(name="Lacida Sans Typewriter", size=text_size)
